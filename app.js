89a96aa48fc10cfb960579e82f339976ac3880a3
daniel <danaphe94@gmail.com>
2025-02-23 20:53:14 -0600

    Agregando las funciones agregar amigo y actualizar lista

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+
+function agregarAmigo(){
+    let texto = document.getElementById("amigo").value.trim();
+
+    if (texto === ""){
+        alert("Campo vacio, por favor, inserte un nombre.");
+    }else {
+        listaAmigos.push(texto);
+        console.log(listaAmigos);
+    }
+    document.querySelector('#amigo').value = '';
+    actualizarLista();
+}
+
+function actualizarLista(){
+    let lista= document.getElementById("listaAmigos");
+    lista.innerHTML="";
+
+    for (let i=0; i< listaAmigos.length ;i++){
+        lista.innerHTML += `<li>${listaAmigos[i]}</li>`;
+    }
+}
